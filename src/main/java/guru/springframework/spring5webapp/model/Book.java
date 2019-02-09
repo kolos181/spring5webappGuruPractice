7e6352821c228a4e9ebb047a770b225645a3ba0e
Kolos <kolos181@gmail.com>
2019-02-09 11:45:32 +0200

    initial commit

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,74 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    private long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    @ManyToMany
+    private Set<Author> auhors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> auhors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.auhors = auhors;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuhors() {
+        return auhors;
+    }
+
+    public void setAuhors(Set<Author> auhors) {
+        this.auhors = auhors;
+    }
+}
